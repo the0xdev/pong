dcda2f0fcd589127985e340ba5bd2fb75cce0580
Imran M <imran@imranmustafa.net>
2024-08-02 18:36:10 -0400

    init commit

---
+++ main.lua
@@ -0,0 +1,7 @@
+-- SPDX-FileCopyrightText: 2024 Imran Mustafa <imran@imranmustafa.net>
+--
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+function love.draw()
+   love.graphics.print("Hello, World!", 400, 300)
+end
